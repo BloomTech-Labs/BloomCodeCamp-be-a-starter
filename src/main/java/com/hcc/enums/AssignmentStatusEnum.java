1a7d885955d69fea1fcc0c295bd5ad0c9abf4d9d
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-04 00:26:57 -0500

    Populated the AssignmentStatusEnum.

--- src/main/java/com/hcc/enums/AssignmentStatusEnum.java
+++ src/main/java/com/hcc/enums/AssignmentStatusEnum.java
@@ -1,4 +1,20 @@
 package com.hcc.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AssignmentStatusEnum {
+    PENDING("Pending"),
+    APPROVED("Approved"),
+    REJECTED("Rejected");
+
+    private String status;
+
+    AssignmentStatusEnum(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
