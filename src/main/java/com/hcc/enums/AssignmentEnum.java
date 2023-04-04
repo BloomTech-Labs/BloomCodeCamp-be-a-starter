d623817717f636a8d72e171247d5012e92601f09
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-04 00:25:16 -0500

    Populated the AssignmentEnum.

--- src/main/java/com/hcc/enums/AssignmentEnum.java
+++ src/main/java/com/hcc/enums/AssignmentEnum.java
@@ -1,4 +1,27 @@
 package com.hcc.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AssignmentEnum {
+    ASSIGNMENT1(1, "Assignment 1"),
+    ASSIGNMENT2(2, "Assignment 2"),
+    ASSIGNMENT3(3, "Assignment 3"),
+    ASSIGNMENT4(4, "Assignment 4");
+
+    private int assignmentNumber;
+    private String assignmentName;
+
+    AssignmentEnum(int assignmentNumber, String assignmentName) {
+        this.assignmentNumber = assignmentNumber;
+        this.assignmentName = assignmentName;
+    }
+
+    public int getAssignmentNumber() {
+        return assignmentNumber;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
 }
