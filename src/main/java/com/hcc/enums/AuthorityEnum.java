d4e87bdbc0c4560a235cca78aa7504dae0b92bf1
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-04 00:27:35 -0500

    Populated the AuthorityEnum.

--- src/main/java/com/hcc/enums/AuthorityEnum.java
+++ src/main/java/com/hcc/enums/AuthorityEnum.java
@@ -1,4 +1,19 @@
 package com.hcc.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum AuthorityEnum {
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private String authority;
+
+    AuthorityEnum(String authority) {
+        this.authority = authority;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
 }
