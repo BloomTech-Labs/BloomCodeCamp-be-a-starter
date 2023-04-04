db3ed02dd3330f22dd8b4cdc4ebc21cf59c93cab
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-03 23:57:46 -0500

    Populated the Authority class
    Put all values, constructors, POJO, and a static Builder class
    for a secondary constructor.

--- src/main/java/com/hcc/entities/Authority.java
+++ src/main/java/com/hcc/entities/Authority.java
@@ -1,4 +1,86 @@
 package com.hcc.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authorities")
 public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "authority")
+    private String authority;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    protected Authority(Long id , String authority , User user) {
+        this.id = id;
+        this.authority = authority;
+        this.user = user;
+    }
+
+    private Authority(Builder builder) {
+        id = builder.id;
+        authority = builder.authority;
+        user = builder.user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private String authority;
+        private User user;
+
+        private Builder() {}
+
+        public Builder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withAuthority(String authority) {
+            this.authority = authority;
+            return this;
+        }
+
+        public Builder withUser(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public Authority build() {
+            return new Authority(this);
+        }
+    }
 }
