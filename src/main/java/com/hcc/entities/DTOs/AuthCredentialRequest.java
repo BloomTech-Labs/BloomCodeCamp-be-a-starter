1025911f68d3379a897dc3c77e36fcc9ab0e048e
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-10 02:04:39 -0500

    Modified both DTOs from a class to a record.
    changed the project structure to java 16 to allow for record classes for
    easy DTO creation and scalability. changed the DTOs from a class object
    to a record object. and modified the assosciated code inside each record
    object.

--- src/main/java/com/hcc/entities/DTOs/AuthCredentialRequest.java
+++ src/main/java/com/hcc/entities/DTOs/AuthCredentialRequest.java
@@ -1,22 +1,7 @@
 package com.hcc.entities.DTOs;
 
-public class AuthCredentialRequest {
-    private String username;
-    private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public record AuthCredentialRequest (
+        String username,
+        String password
+){
 }
