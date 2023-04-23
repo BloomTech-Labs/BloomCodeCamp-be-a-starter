49afe1a5e6af63ec707d40dbde0566f61f428099
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-22 20:00:35 -0500

    Created a custom exception.

--- src/main/java/com/hcc/exceptions/UserNotFoundException.java
+++ src/main/java/com/hcc/exceptions/UserNotFoundException.java
@@ -1,2 +1,20 @@
-package com.hcc.exceptions;public class UserNotFoundException {
+package com.hcc.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = -2333404698891013429L;
+
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message , Throwable cause) {
+        super(message , cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
