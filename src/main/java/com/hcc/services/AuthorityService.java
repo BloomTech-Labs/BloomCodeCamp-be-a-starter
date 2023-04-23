dbe34b4267e9915755b7e85115b51061f3254212
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-22 20:05:38 -0500

    created a new service for Authority entitiy.

--- src/main/java/com/hcc/services/AuthorityService.java
+++ src/main/java/com/hcc/services/AuthorityService.java
@@ -1,2 +1,25 @@
-package com.hcc.services;public class AuthorityService {
+package com.hcc.services;
+
+import com.hcc.entities.Authority;
+import com.hcc.entities.User;
+import com.hcc.exceptions.UserNotFoundException;
+import com.hcc.repositories.AuthorityRepository;
+import com.hcc.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthorityService {
+
+    @Autowired
+    AuthorityRepository authorityRepo;
+
+    @Autowired
+    UserRepository userRepo;
+
+    public Optional<User> validateUser(String username, String password) {
+        return userRepo.findByUsernameAndPassword(username, password);
+    }
 }
