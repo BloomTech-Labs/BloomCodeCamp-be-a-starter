98d7371b07c5f074f73bd6cb8b2dd2b7e9ee758a
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-26 01:14:44 -0500

    created a login tester.

--- src/test/java/com/hcc/LoginControllerTest.java
+++ src/test/java/com/hcc/LoginControllerTest.java
@@ -1,2 +1,31 @@
-package com.hcc;public class LoginControllerTest {
+package com.hcc;
+
+import com.hcc.controllers.LoginController;
+import com.hcc.entities.DTOs.AuthCredentialRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+public class LoginControllerTest {
+//    private AuthCredentialRequest request;
+//
+//    @BeforeEach
+//    protected void setup() {
+//        request.setUsername("jhon");
+//        request.setPassword("password");
+//    }
+
+    private LoginController controller = new LoginController();
+
+    @Test
+    public void check() throws Exception {
+        AuthCredentialRequest request = AuthCredentialRequest.builder()
+                .withUsername("jhon")
+                .withPassword("password")
+                .build();
+
+        ResponseEntity<?> response = controller.login(request);
+
+        System.out.println(response.toString());
+    }
 }
