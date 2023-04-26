aa9b463d94f84199bf71cdaa3ec3ac7f544a681b
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-26 01:48:29 -0500

    removed accidental colon.

--- src/main/java/com/hcc/utils/JwtFilter.java
+++ src/main/java/com/hcc/utils/JwtFilter.java
@@ -1,6 +1,6 @@
 package com.hcc.utils;
 
-import com.hcc.repositories.UserRepository;:
+import com.hcc.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
