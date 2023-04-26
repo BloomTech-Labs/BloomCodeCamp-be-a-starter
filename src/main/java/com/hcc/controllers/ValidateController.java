837492329ca42b32ab6ea51b0cedd7b12d811d51
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-26 01:27:53 -0500

    Added a validateToken endpoint/method

--- src/main/java/com/hcc/controllers/ValidateController.java
+++ src/main/java/com/hcc/controllers/ValidateController.java
@@ -1,2 +1,26 @@
-package com.hcc.controllers;public class ValidateController {
+package com.hcc.controllers;
+
+import com.hcc.entities.User;
+import com.hcc.utils.JWtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ValidateController {
+    @Autowired
+    private JWtUtils utils;
+
+    @GetMapping(value = "/api/aut/validate", consumes = {"application/json"}, produces = {"application/json"})
+    ResponseEntity<?> validateToken(@RequestParam String token, @AuthenticationPrincipal User user) {
+        boolean valid = utils.validateToken(token, (UserDetails) user);
+        return new ResponseEntity<>(valid, HttpStatus.OK);
+    }
 }
