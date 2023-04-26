51ce8ecba04bae3e036821985640c91195637761
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-26 01:33:22 -0500

    implemented GrantedAuthority interface.

--- src/main/java/com/hcc/entities/Authority.java
+++ src/main/java/com/hcc/entities/Authority.java
@@ -1,10 +1,12 @@
 package com.hcc.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "authorities")
-public class Authority {
+public class Authority implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
