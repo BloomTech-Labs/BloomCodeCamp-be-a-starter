0b01019895aed96fdaba034958ed0b4279faf089
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-04-26 01:41:51 -0500

    Created and populated DataLoader class
    made class implment CommandLineRunner along with creating 2 classes run
    and loadUserData to populate my user repo in the event my repo is empty.
    this class is mainly for debugging purposes.

--- src/main/java/com/hcc/utils/DataLoader.java
+++ src/main/java/com/hcc/utils/DataLoader.java
@@ -1,2 +1,37 @@
-package com.hcc.utils;public class DataLoader {
+package com.hcc.utils;
+
+import com.hcc.entities.User;
+import com.hcc.repositories.AuthorityRepository;
+import com.hcc.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AuthorityRepository authorityRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadUserData();
+    }
+
+    private void loadUserData() {
+        if (userRepository.count() == 0) {
+            PasswordEncoder encoder = new BCryptPasswordEncoder();
+            String password = encoder.encode("password");
+            User user = new User.Builder().withCohortStartDate(LocalDate.now())
+                    .withUsername("joe")
+                    .withPassword(password)
+                    .build();
+            userRepository.save(user);
+        }
+    }
 }
