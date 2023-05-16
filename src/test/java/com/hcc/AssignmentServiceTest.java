f56aab1fd3574ef157ceef8ed3dcfab2b4f00c54
Andrew Lam <andrew.x.li.lam@gmail.com>
2023-05-16 14:34:03 -0500

    added test for Assignment and User.

--- src/test/java/com/hcc/AssignmentServiceTest.java
+++ src/test/java/com/hcc/AssignmentServiceTest.java
@@ -1,2 +1,89 @@
-package com.hcc;public class AssignmentServiceTest {
+package com.hcc;
+
+import com.hcc.entities.Assignment;
+import com.hcc.entities.User;
+import com.hcc.services.AssignmentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class AssignmentServiceTest {
+
+    @Autowired
+    AssignmentService service;
+
+    @Test
+    void loadListOfAssignments_usingValidUser() {
+        // GIVEN
+        User user = User.builder()
+                .withUsername("jhon")
+                .withPassword("password")
+                .build();
+        // WHEN
+        Optional<List<Assignment>> assignments = service.loadAssignmentsByUser(user);
+
+        // THEN
+        assertAll(
+                () -> assertNotNull(assignments, "Expected assignments to not be null"),
+                () -> assertFalse(assignments.isEmpty(), "Expected assignments not to be empty"),
+                () -> assertEquals(user.getUsername(),
+                        assignments.get().get(0).getUser().getUsername(),
+                        "Expected assignment to match with given User")
+        );
+    }
+    @Test
+    void loadListOfAssignments_usingValidAssignmentId() {
+        // GIVEN
+        Long id = 1L;
+
+        // WHEN
+        Optional<Assignment> assignments = service.loadAssignmentById(id);
+
+        // THEN
+        assertAll(
+                () -> assertNotNull(assignments, "Expected assignments to not be null"),
+                () -> assertFalse(assignments.isEmpty(), "Expected assignments not to be empty")
+        );
+    }
+    @Test
+    void saveNewAssignment_givenValidAssignment() {
+        // GIVEN
+        User user  = User.builder()
+                .withUsername("jhon")
+                .withPassword("password")
+                .build();
+
+        Assignment assignment = Assignment.builder()
+                .withNumber(1)
+                .withUser(user)
+                .build();
+
+        // WHEN/THEN
+        verify(service).save(assignment);
+
+    }
+    @Test
+    void deleteAssignment_givenValidAssignment() {
+        // GIVEN
+        User user  = User.builder()
+                .withUsername("jhon")
+                .withPassword("password")
+                .build();
+
+        Assignment assignment = Assignment.builder()
+                .withNumber(1)
+                .withUser(user)
+                .build();
+
+        // WHEN/THEN
+        verify(service).delete(assignment);
+    }
 }
--- src/test/java/com/hcc/UserDetailServiceTest.java
+++ src/test/java/com/hcc/UserDetailServiceTest.java
@@ -1,2 +1,44 @@
-package com.hcc;public class UserDetailServiceTest {
+package com.hcc;
+
+import com.hcc.services.UserDetailServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class UserDetailServiceTest {
+
+    @Autowired
+    UserDetailServiceImpl service;
+
+    @Test
+    void giveValidUsername_returnsValidUserDetails() {
+        // GIVEN
+        String validUsername = "jhon";
+        Long id = 1L;
+
+        // WHEN
+        UserDetails userDetails = service.loadUserByUsername(validUsername);
+
+        // THEN
+        assertEquals(userDetails.getUsername(), validUsername,
+                        String.format("Expected a valid username of: %s, but got %s",
+                                validUsername, userDetails.getUsername()));
+    }
+
+    @Test
+    void giveInvalidUsername_throwsUserNotFoundException() {
+        // GIVEN
+        String invalidUsername = "invalidUsername";
+
+        // WHEN/THEN
+        assertThrows(UsernameNotFoundException.class,
+                () -> service.loadUserByUsername(invalidUsername),
+                "Expected method to throw an exception if a user cannot be found.");
+    }
 }
